c094b36527293d984128176c900768a7e2f1e7c4
edgarp1009 <edgarp1009@gmail.com>
2024-04-18 10:19:39 +0200

    agregue algunas imagenes para cuando se gana la partida

--- main.js
+++ main.js
@@ -10,30 +10,21 @@ const blocker = document.querySelector(".blocker");
 
 const resultsCont = document.querySelector(".results");
 const happy = document.querySelector(".happy");
+const tie = document.querySelector(".tie");
 const sad = document.querySelector(".sad");
 const youWin = document.querySelector(".you-win");
 const youLose = document.querySelector(".you-lose");
 
-setTimeout(()=>{
-    start.addEventListener("click", ()=>{
-        startGame.classList.add("hidden")
-        logo.classList.add("hidden");
-        play.classList.remove("hidden");
-    })
-    classic.addEventListener("click", ()=>{
-        sheldon.classList.toggle("hidden");
-        start.classList.toggle("hidden");
-    });
-    sheldon.addEventListener("click", ()=>{
-        classic.classList.toggle("hidden");
-        start.classList.toggle("hidden");
-    });
-    loader.classList.add("hidden");
-    game.classList.remove("hidden")
-
-},3500)
-
-//OPTIONS
+const rock2 = document.querySelector(".rock2");
+const paper2 = document.querySelector(".paper2");
+const scissors2 = document.querySelector(".scissors2");
+const lizard2 = document.querySelector(".lizard2");
+const spock2 = document.querySelector(".spock2");
+const interrogation = document.querySelector(".interrogation");
+const score1 = document.querySelector(".score1");
+const score2 = document.querySelector(".score2");
+let points1 = 0;
+let points2 = 0;
 
 let player1="";
 let player2="";
@@ -43,6 +34,31 @@ const paper1 = document.querySelector(".paper1");
 const scissors1 = document.querySelector(".scissors1");
 const lizard1 = document.querySelector(".lizard1");
 const spock1 = document.querySelector(".spock1");
+
+score1.innerHTML=`YOU: ${points1}`;
+score2.innerHTML=`SHELDON: ${points2}`;
+
+start.addEventListener("click", ()=>{
+    startGame.classList.add("hidden")
+    logo.classList.add("hidden");
+    play.classList.remove("hidden");
+})
+classic.addEventListener("click", ()=>{
+    sheldon.classList.toggle("hidden");
+    start.classList.toggle("hidden");
+});
+sheldon.addEventListener("click", ()=>{
+    classic.classList.toggle("hidden");
+    start.classList.toggle("hidden");
+});
+
+setTimeout(()=>{
+    
+    loader.classList.add("hidden");
+    game.classList.remove("hidden")
+
+},3500)
+
 rock1.addEventListener("click", ()=>{
     paper1.classList.add("hidden");
     scissors1.classList.add("hidden");
@@ -68,25 +84,12 @@ scissors1.addEventListener("click", ()=>{
     battle();
 });
 
-const rock2 = document.querySelector(".rock2");
-const paper2 = document.querySelector(".paper2");
-const scissors2 = document.querySelector(".scissors2");
-const lizard2 = document.querySelector(".lizard2");
-const spock2 = document.querySelector(".spock2");
-const interrogation = document.querySelector(".interrogation");
-const score1 = document.querySelector(".score1");
-const score2 = document.querySelector(".score2");
-let points1 = 0;
-let points2 = 0;
-
-score1.innerHTML=`YOU: ${points1}`;
-score2.innerHTML=`SHELDON: ${points2}`;
-
 function battle() {
 
     pcChoice();
     if(player1 == player2){
         console.log("empate");
+        tiedOne();
 
     } else if (player1 == "rock" && player2 == "scissors"){
         console.log("ganaste");
@@ -123,22 +126,29 @@ function battle() {
     score1.innerHTML=`YOU: ${points1}`;
     score2.innerHTML=`SHELDON: ${points2}`;
 
-    setTimeout(restart, 2700);
+    setTimeout(restart, 3000);
 }
 
 function wonOne() {
     setTimeout(()=>{
         happy.classList.remove("hidden");
         resultsCont.classList.remove("hidden");
-    },700);
+    },1000);
 }
 
 function lostOne() {
     setTimeout(()=>{
         sad.classList.remove("hidden");
         resultsCont.classList.remove("hidden");
-    },700);
+    },1000);
     
+}
+
+function tiedOne() {
+    setTimeout(()=>{
+        tie.classList.remove("hidden");
+        resultsCont.classList.remove("hidden");
+    },1000);
 }
 
 function pcChoice() {
@@ -163,29 +173,70 @@ function pcChoice() {
 function restart() {
     const pcChoice = document.querySelectorAll(".choice");
     const playerChoice = document.querySelectorAll(".playerChoice");
-    pcChoice.forEach(element => {
-        if(!element.classList.contains("hidden")){
-            element.classList.add("hidden");
-        }
-    });
-    playerChoice.forEach(element => {
-        if(element.classList.contains("hidden")){
-            element.classList.remove("hidden");
-        }
-    });
+
+    if(points1 < 3 && points2 < 3) {
+
+        pcChoice.forEach(element => {
+            if(!element.classList.contains("hidden")){
+                element.classList.add("hidden");
+            }
+        });
+        playerChoice.forEach(element => {
+            if(element.classList.contains("hidden")){
+                element.classList.remove("hidden");
+            }
+        });
+        
+        interrogation.classList.remove("hidden");
+        blocker.classList.add("hidden");
+        resultsCont.classList.add("hidden");
     
-    interrogation.classList.remove("hidden");
-    blocker.classList.add("hidden");
-    resultsCont.classList.add("hidden");
-
-    if(!happy.classList.contains("hidden")){
-        happy.classList.add("hidden");
-    } else if (!sad.classList.contains("hidden")){
-        sad.classList.add("hidden");
-    }
-
-    if (points1 == 3 || points2 == 3){
-        resetGame();
+        if(!happy.classList.contains("hidden")){
+            happy.classList.add("hidden");
+        } else if (!sad.classList.contains("hidden")){
+            sad.classList.add("hidden");
+        } else if (!tie.classList.contains("hidden")){
+            tie.classList.add("hidden");
+        }
+    } else if (points1 == 3 || points2 == 3){
+        if(!happy.classList.contains("hidden")){
+            happy.classList.add("hidden");
+        } else if (!sad.classList.contains("hidden")){
+            sad.classList.add("hidden");
+        } else if (!tie.classList.contains("hidden")){
+            tie.classList.add("hidden");
+        }
+        if (points1 == 3) {
+            // interrogation.classList.remove("hidden");
+            resultsCont.classList.remove("hidden");
+            youWin.classList.remove("hidden");
+            setTimeout(()=>{
+                resultsCont.classList.add("hidden");
+                youWin.classList.add("hidden");
+            },1500);
+        } else if (points2 == 3) {
+            resultsCont.classList.remove("hidden");
+            youLose.classList.remove("hidden");
+            setTimeout(()=>{
+                resultsCont.classList.add("hidden");
+                youLose.classList.add("hidden");
+            },1500);
+        }
+        setTimeout(resetGame, 1500);
+        setTimeout(()=>{
+            interrogation.classList.remove("hidden");
+            blocker.classList.add("hidden");
+            pcChoice.forEach(element => {
+                if(!element.classList.contains("hidden")){
+                    element.classList.add("hidden");
+                }
+            });
+            playerChoice.forEach(element => {
+                if(element.classList.contains("hidden")){
+                    element.classList.remove("hidden");
+                }
+            });
+        }, 2000);
     }
 }
 
